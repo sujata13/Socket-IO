deb9d85bacdeb4dec7a268d06f34bd953bdc8f71
Sujata Mishra <mishrasujata1628@gmail.com>
2020-06-25 13:06:01 +0000

    All task done

--- src/app.js
+++ src/app.js
@@ -1,36 +1,27 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const http = require('http').Server(app)
+const http = require("http").Server(app);
 
+app.set("view engine", "ejs");
 
+app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-
-//set the template engine ejs
-app.set('view engine', 'ejs')
-
-//path to find the static content
-app.use(express.static('public'))
-
-
-//Routing function which denotes that whenever there is get request on the server,
-//it must send the index.ejs file(which we will be creating later) in response.
-app.get('/', (req, res) => {
-	res.render('index.html')
-})
-
-//Listen on port 3000
 server = http.listen(3000);
 
 const io = require("socket.io")(server);
 
-//listen on every connection
-io.on('connect', (socket) => {               //Line1
-	console.log('New user connected')
+io.on("connection", socket => {
+  console.log("a user connected");
+  socket.on("chat_message", data => {
+    io.sockets.emit("chat_message", data);
+  });
+});
 
-    //listen on new_message
-    socket.on('new_message', (data) => {        //Line2
-        //broadcast the new message
-        io.sockets.emit('new_message', data);   //Line3
-    })  
-})
+module.exports = {
+  server: server,
+  http_: http
+};
